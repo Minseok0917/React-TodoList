55c79a737ce16d3ae3a71cf123232a03a067a645
Minseok0917 <jhs749636@naver.com>
2020-10-10 02:40:06 +0900

    Updates

---
+++ precache-manifest.81d651dce1351173531ca7626fb6d44e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9da28d47aa70857f6fa0eb4012353a3b",
+    "url": "/React-TodoList/index.html"
+  },
+  {
+    "revision": "da9c615f80af1b2bf9e3",
+    "url": "/React-TodoList/static/js/2.722e80bb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-TodoList/static/js/2.722e80bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c23fd81d77c6ea2e51ee",
+    "url": "/React-TodoList/static/js/main.ff3805fa.chunk.js"
+  },
+  {
+    "revision": "1659d061a0ddddc1b30c",
+    "url": "/React-TodoList/static/js/runtime-main.72e65d22.js"
+  }
+]);
